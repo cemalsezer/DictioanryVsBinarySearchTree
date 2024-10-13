eb1adb12fd9c10c53076880f06ae92a011473bcc
CemalSezer <infocemalsezer@gmail.com>
2024-10-13 14:35:24 +0300

    Add project files.

---
+++ CustomDictionary.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryVsBinarySearchTree
+{
+    public class CustomDictionary
+    {
+        private const int Size = 1000000;  // 1 milyon veri kapasitesi
+        private LinkedList<KeyValuePair<int, int>>[] items;
+
+        public CustomDictionary()
+        {
+            items = new LinkedList<KeyValuePair<int, int>>[Size];
+        }
+
+        private int GetIndex(int key)
+        {
+            return key % Size;
+        }
+
+        public void Insert(int key, int value)
+        {
+            int index = GetIndex(key);
+
+            if (items[index] == null)
+            {
+                items[index] = new LinkedList<KeyValuePair<int, int>>();
+            }
+
+            // Anahtarın zaten var olup olmadığını kontrol edin
+            foreach (var pair in items[index])
+            {
+                if (pair.Key == key)
+                {
+                    // Anahtar zaten varsa, ekleme işlemi yapmayın.
+                    return;  // Hata fırlatmak yerine geri dönün
+                }
+            }
+
+            items[index].AddLast(new KeyValuePair<int, int>(key, value));
+        }
+
+        public bool Search(int key)
+        {
+            int index = GetIndex(key);
+
+            if (items[index] != null)
+            {
+                foreach (var pair in items[index])
+                {
+                    if (pair.Key == key)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Manager.cs
@@ -0,0 +1,41 @@
+﻿using DictionaryVsBinarySearchTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictioanryVsBinarySearchTree
+{
+    public class Manager
+    {
+        private CustomDictionary customDict;
+        private BinarySearchTree bst;
+
+        public Manager()
+        {
+            customDict = new CustomDictionary();
+            bst = new BinarySearchTree();
+        }
+
+        public void InsertToDictionary(int key, int value)
+        {
+            customDict.Insert(key, value);
+        }
+
+        public void InsertToBST(int key)
+        {
+            bst.Insert(key);
+        }
+
+        public bool SearchInDictionary(int key)
+        {
+            return customDict.Search(key);
+        }
+
+        public bool SearchInBST(int key)
+        {
+            return bst.Search(key);
+        }
+    }
+}
